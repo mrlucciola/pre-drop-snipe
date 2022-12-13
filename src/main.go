deab2644a9cec41c9429c1c27745912743e7e87a
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2022-12-13 17:50:24 -0500

    feat: #3 fetch collection info from opensea
    this does NOT yet get all assets in the collection

--- src/main.go
+++ src/main.go
@@ -1,4 +1,12 @@
 package main
 
+import "log"
+
+// https://opensea.io/collection/mutant-ape-yacht-club
+var collectionSlug = "mutant-ape-yacht-club"
+
 func main() {
+	client := httpClient()
+	res := getOsCollection(client, collectionSlug)
+	log.Println("Res body:", string(res))
 }
