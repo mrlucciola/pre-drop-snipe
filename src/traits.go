d3a81839b6bd6d1a70786ad2506cd688c537fba8
Rocco Lucciola <47836741+mrlucciola@users.noreply.github.com>
2022-12-15 02:40:25 -0500

    feat: #21 sort tokens by rarity, display top 5 (#22)

--- src/traits.go
+++ src/traits.go
@@ -1,6 +1,6 @@
 package main
 
-// Distribution map for trait values of a single group
+// Frequency map for trait values of a single group
 //
 // Shows the occurrences of each trait.
 //
@@ -12,11 +12,11 @@ package main
 //	  "m1 bored unshaven": 2257,
 //	  "m1 dumbfounded":    750,
 //	}
-type TraitValueDistMap map[string]int
+type TraitValueFreqMap map[string]int
 
 // Probability map for trait values of a single group.
 //
-// Trait value probabilities are calculated from the distribution mapping.
+// Trait value probabilities are calculated from the frequency mapping.
 //
 //	Mouth: {
 //	  "m2 bored":          0.1355 ( 991 / 7311),
@@ -26,15 +26,17 @@ type TraitValueDistMap map[string]int
 //	}
 type TraitValueProbMap map[string]float64
 
-type TraitDistributionMap map[string]TraitValueDistMap
+type TraitFrequencyMap map[string]TraitValueFreqMap
 
 // Probabilities for all traits
 type TraitProbabilityMap map[string]TraitValueProbMap
 
-// Calculate trait value probabilities, by group
+// DEPRECATED
 //
-// Input the distribution for the trait values of a single group & return its probability mapping
-func calcTraitValueProbsByGroup(traitGroup TraitValueDistMap, activeTokenCount int) TraitValueProbMap {
+// # Calculate trait value probabilities, by group
+//
+// Input the frequency for the trait values of a single group & return its probability mapping
+func calcTraitValueProbsByGroup(traitGroup TraitValueFreqMap, activeTokenCount int) TraitValueProbMap {
 	// get sum of all occurences
 	var traitOccurrenceSum int
 
@@ -54,6 +56,7 @@ func calcTraitValueProbsByGroup(traitGroup TraitValueDistMap, activeTokenCount i
 	return traitGroupProb
 }
 
+// DEPRECATED: update test
 // Calculate the probabilities for all traits for the token collection in its current state.
 //
 // Creates a probability mapping of all traits, nested within their respective groups.
@@ -61,7 +64,7 @@ func calcTraitValueProbsByGroup(traitGroup TraitValueDistMap, activeTokenCount i
 // Sources info from Skip Protocol's database.
 //
 // Note: OpenSea has a lack of consistency for contract versions with Akuri.
-func calcAllTraitValueProbs(traits CollectionTraits, activeTokenCount int) TraitProbabilityMap {
+func calcAllTraitValueProbs(traits TraitFrequencyMap, activeTokenCount int) TraitProbabilityMap {
 	probMap := TraitProbabilityMap{}
 
 	// TODO: parallelize
@@ -72,31 +75,46 @@ func calcAllTraitValueProbs(traits CollectionTraits, activeTokenCount int) Trait
 	return probMap
 }
 
-/*
-Iterate through the list of tokens pulled from the Skip Protocol API
-  - Input is `Token` array - from Skip Protocol API
-*/
-func getAllTraitStatsSkip(tokenArr []*Token, activeTokenCount int) TraitProbabilityMap {
-	traitProbabilities := TraitProbabilityMap{}
-	traitOccurences := TraitDistributionMap{}
+// Build the trait frequency map
+// TODO: parallelize if possible
+func buildTraitFrequencyMap(tokenArr []*Token) TraitFrequencyMap {
+	traitOccurences := TraitFrequencyMap{}
 
-	// TODO: parallelize if possible
 	for _, token := range tokenArr {
 
 		// iterate thru the traits, add occurences to the map
 		for traitGroup, traitValue := range token.traits {
 			if _, found := traitOccurences[traitGroup]; !found {
-				traitOccurences[traitGroup] = TraitValueDistMap{}
+				traitOccurences[traitGroup] = TraitValueFreqMap{}
 			}
 
 			traitOccurences[traitGroup][traitValue] += 1
 		}
 	}
+	return traitOccurences
+}
+
+/*
+Build a mapping of all probabilities for all possible traits assignable to a token.
+
+The map is a nested data structure that holds
+  - "Trait Groups" at the top level
+  - "Trait Values" within trait groups
+
+Iterate through the list of tokens pulled from the Skip Protocol API and
+build a frequency map for each trait as they appear.
+  - Input is `Token` array - from Skip Protocol API
+*/
+func buildTraitProbabilityMap(tokenArr []*Token, activeTokenCount int) TraitProbabilityMap {
+
+	traitOccurences := buildTraitFrequencyMap(tokenArr)
 
 	// TODO: parallelize if possible
+	traitProbabilities := TraitProbabilityMap{}
 	for traitGroup, traitValueMap := range traitOccurences {
 		// sum all value counts for the group
 		groupSum := 0
+		// TODO: parallelize if possible
 		for _, valueCt := range traitValueMap {
 			groupSum += valueCt
 		}
@@ -107,29 +125,11 @@ func getAllTraitStatsSkip(tokenArr []*Token, activeTokenCount int) TraitProbabil
 		}
 
 		// iter thru each value, divide to get prob for each value, assign to map
+		// TODO: parallelize if possible
 		for traitValue, valueCt := range traitValueMap {
 			traitProbabilities[traitGroup][traitValue] = float64(valueCt) / float64(groupSum)
 		}
 	}
 
 	return traitProbabilities
-}
-
-// Calculate the probability for an individual token.
-//
-// Call after any update to token datastore, or token datastore init
-func calculateRarity(tokenToEval Token, probabilityMap TraitProbabilityMap, tokenRarityArr []float64) float64 {
-	// initialize at 1
-	prob := 1.
-
-	// iterate thru each trait on the token
-	for traitGroup, traitValue := range tokenToEval.traits {
-		// look up value in the map
-		prob *= probabilityMap[traitGroup][traitValue]
-	}
-
-	// add to datastore
-	tokenRarityArr[tokenToEval.id] = roundFloat(prob, 15)
-
-	return prob
 }
--- src/utils.go
+++ src/utils.go
@@ -1,8 +1,14 @@
 package main
 
-import "math"
+import (
+	"log"
+	"math"
+	"os"
+)
 
 func roundFloat(val float64, precision uint) float64 {
 	ratio := math.Pow(10, float64(precision))
 	return math.Round(val*ratio) / ratio
 }
+
+var logger *log.Logger = log.New(os.Stdout, "", log.Ldate|log.Ltime)
