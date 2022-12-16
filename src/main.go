902ce254c79a7b8ed43beb514cfc07d47fde20c4
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2022-12-16 14:02:22 -0500

    fix: #27 fix concurrent access with add rwMutex

--- src/main.go
+++ src/main.go
@@ -25,6 +25,7 @@ func main() {
 		freqMap = getTokens(tokenSlug, tokens)
 	}
 
+	fmt.Println("\n\ngetting rarity\n ")
 	var tokenRarityArr []TokenRarity
 	useRarityScore := true
 	if useRarityScore {
@@ -37,9 +38,11 @@ func main() {
 	}
 
 	// sort
+	fmt.Println("\n\nsorting tokens\n ")
 	sortedArr := sortRarityArr(tokenRarityArr)
 
 	// Display the top five
+	fmt.Println("\n\ndisplaying tokens\n ")
 	for _, v := range sortedArr[:5] {
 		fmt.Println(v.id, v.rarity.StringFixed(20))
 	}
